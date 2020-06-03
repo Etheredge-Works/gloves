d8753070246aa1ea6612ec0d121d376116bea191
Ben <benjamin.etheredge@gmail.com>
2020-06-03 15:42:15 -0500

    Fixed get_labels test. Cleaning up tests.

--- test_utils.py
+++ test_utils.py
@@ -1,9 +1,9 @@
+import os
+# Don't want to use GPUs for unit tests as this will cause various machine issues
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 import pytest
 import utils
-import os
 import pathlib
-import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 import tensorflow as tf
 def test_get_dataset():
     data_dir = utils.get_dataset()
@@ -16,6 +16,17 @@ def test_get_dataset():
             assert file.endswith("jpg") # Don't want any of those dumb mat files
             assert os.path.getsize(data_dir/file) > 0, f"{file} is empty."
 
+def test_get_label():
+    s = os.path.sep
+    paths = [
+        (f"testing{s}this{s}path{s}thingy{s}asdfasdf_label_0.jpg", 'asdfasdf_label'),
+        (f"testing{s}this{s}path{s}thingy{s}asdfasdf_label_1.jpg", 'asdfasdf_label'),
+        (f"{s}testing{s}path{s}thingy{s}asdfasdf_label_2.jpg", 'asdfasdf_label')
+    ]
+    for file, label in paths:
+        predicted_label = utils.get_label(file)
+        assert predicted_label == label, f"Incorrect label: Got {predicted_label} instead of {label}"
+
 @pytest.mark.parametrize("file", [str(utils.DATA_DIR/file) for file in os.listdir(utils.DATA_DIR)])
 def test_get_pairs(file):
     #dataset_dir = utils.get_dataset()
@@ -25,26 +36,11 @@ def test_get_pairs(file):
     anchor_label = utils.get_label(anchor_file)
     assert anchor_label == old_label
     other_label = utils.get_label(positive_file)
-    assert (anchor_label == other_label) == bool(label)
-    print(anchor_file)
-    print(positive_file)
-    print(label)
-    #assert anchor_label != negative_label, "Anchor and negative match."
+    assert (anchor_label == other_label) == bool(label), \
+        f"Labels are incorrect: {anchor_label} - {other_label}\nlabel: {label}"
 
     # TODO do we want to test that the files are different?
     assert tf.convert_to_tensor(str(anchor_file)) != positive_file, "Anchor and positive are the same image."
-
-"""
-def test_get_label():
-    paths = [
-        "testing/this/path/thingy/asdfasdf_label_0.jpg",
-        "testing\\this\\path\\thingy\\asdfasdf_label_1.jpg",
-        "/testing/this/path/thingy/asdfasdf_label_2.jpg"
-    ]
-    for idx, file in enumerate(paths):
-        predicted_label = utils.get_label(file)
-        assert predicted_label == f"label", f"Incorrect label: Got {predicted_label} instead of label{idx}"
-"""
 
 
 def test_simple_decode():
