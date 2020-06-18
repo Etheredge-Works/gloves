cc99d8fc00076f6ec36aa72e611adb966591e7e5
Ben <benjamin.etheredge@gmail.com>
2020-06-18 15:38:12 -0500

    Configuring code to clean up Oxford pet dataset since it contains incorrectly named images (e.g. gif->jpg).

--- src/convert_images.py
+++ src/convert_images.py
@@ -4,39 +4,49 @@ import sys
 
 import tensorflow as tf
 import PIL
+import pathlib
 
+# https://github.com/tensorflow/models/issues/2194
 def main(argv):
-    path_images = './images'
-    filenames_src = tf.gfile.ListDirectory(path_images)
+    path_images =pathlib.Path('data/images')
+    new_path = pathlib.Path('data/cleaned_images')
+    if not new_path.exists():
+        new_path.mkdir()
+
+    filenames_src = tf.io.gfile.listdir(str(path_images))
     for filename_src in filenames_src:
         stem, extension = os.path.splitext(filename_src)
         if (extension.lower() != '.jpg'): continue
 
-        pathname_jpg = '{}/{}'.format(path_images, filename_src)
-        with tf.gfile.GFile(pathname_jpg, 'rb') as fid:
+        pathname_jpg = str(path_images/filename_src)
+        new_pathname_jpg = str(new_path/filename_src)
+        with tf.io.gfile.GFile(pathname_jpg, 'rb') as fid:
             encoded_jpg = fid.read(4)
         # png
         if(encoded_jpg[0] == 0x89 and encoded_jpg[1] == 0x50 and encoded_jpg[2] == 0x4e and encoded_jpg[3] == 0x47):
             # copy jpg->png then encode png->jpg
             print('png:{}'.format(filename_src))
-            pathname_png = '{}/{}.png'.format(path_images, stem)
-            tf.gfile.Copy(pathname_jpg, pathname_png, True)
-            PIL.Image.open(pathname_png).convert('RGB').save(pathname_jpg, "jpeg")
+            pathname_png = f'{path_images}{os.path.sep}{stem}.png'
+            tf.io.gfile.copy(pathname_jpg, pathname_png, True)
+            PIL.Image.open(pathname_png).convert('RGB').save(new_pathname_jpg, "jpeg")
         # gif
         elif(encoded_jpg[0] == 0x47 and encoded_jpg[1] == 0x49 and encoded_jpg[2] == 0x46):
             # copy jpg->gif then encode gif->jpg
             print('gif:{}'.format(filename_src))
-            pathname_gif = '{}/{}.gif'.format(path_images, stem)
-            tf.gfile.Copy(pathname_jpg, pathname_gif, True)
-            PIL.Image.open(pathname_gif).convert('RGB').save(pathname_jpg, "jpeg")
+            pathname_gif = f'{path_images}{os.path.sep}{stem}.gif'
+            tf.io.gfile.copy(pathname_jpg, pathname_gif, True)
+            PIL.Image.open(pathname_gif).convert('RGB').save(new_pathname_jpg, "jpeg")
         elif(filename_src == 'beagle_116.jpg' or filename_src == 'chihuahua_121.jpg'):
             # copy jpg->jpeg then encode jpeg->jpg
             print('jpeg:{}'.format(filename_src))
-            pathname_jpeg = '{}/{}.jpeg'.format(path_images, stem)
-            tf.gfile.Copy(pathname_jpg, pathname_jpeg, True)
-            PIL.Image.open(pathname_jpeg).convert('RGB').save(pathname_jpg, "jpeg")
+            pathname_jpeg = f'{path_images}{os.path.sep}{stem}.jpeg'
+            tf.io.gfile.copy(pathname_jpg, pathname_jpeg, True)
+            PIL.Image.open(pathname_jpeg).convert('RGB').save(new_pathname_jpg, "jpeg")
         elif(encoded_jpg[0] != 0xff or encoded_jpg[1] != 0xd8 or encoded_jpg[2] != 0xff):
             print('not jpg:{}'.format(filename_src))
+        else:
+            tf.io.gfile.copy(pathname_jpg, new_pathname_jpg, True)
+
 
 if __name__ == "__main__":
     sys.exit(int(main(sys.argv) or 0))
