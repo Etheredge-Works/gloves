72875a0a2ad3bfff312352c795927fbbdfc2993f
Benjamin-Etheredge <benjamin.etheredge@gmail.com>
2020-12-24 20:26:04 -0600

    cleaned up

--- build_image.sh
+++ build_image.sh
@@ -1,2 +1,11 @@
-#! /bin/sh
-docker build -t etheredgeb/gloves:latest . && docker push etheredgeb/gloves:latest
+#!/bin/bash -e
+image_name=etheredgeb/gloves
+image_tag=0.2
+full_image_name=${image_name}:${image_tag}
+
+cd "$(dirname "$0")" 
+docker build -t "${full_image_name}" .
+docker push "$full_image_name"
+
+# Output the strict image name (which contains the sha256 image digest)
+docker inspect --format="{{index .RepoDigests 0}}" "${full_image_name}"
