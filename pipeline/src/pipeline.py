028dc5f2ac2b3cb35bc3e50c73b610378034d464
ben <benjamin.etheredge@gmail.com>
2021-01-19 12:11:30 -0600

    current working version

--- pipeline/src/pipeline.py
+++ pipeline/src/pipeline.py
@@ -12,44 +12,75 @@ def load_component(dir_name):
     print(f"Loading {filename}")
     return kfp.components.load_component_from_url(filename)
 
+def load_local_component(dir_name):
+    filename = os.path.join('component.yaml')
+    print(f"Loading {filename}")
+    return kfp.components.load_component_from_url(filename)
 
-# Load two more components for importing and exporting the data:
-#download_from_gcs_op = kfp.components.load_component_from_url('http://....../component.yaml')
-#upload_to_gcs_op = kfp.components.load_component_from_url('http://....../component.yaml')
+def add_env(op, var):
+    pass
 
-# dummy_op is now a "factory function" that accepts the arguments for the component's inputs
-# and produces a task object (e.g. ContainerOp instance).
-# Inspect the dummy_op function in Jupyter Notebook by typing "dummy_op(" and pressing Shift+Tab
-# You can also get help by writing help(dummy_op) or dummy_op? or dummy_op??
-# The signature of the dummy_op function corresponds to the inputs section of the component.
-# Some tweaks are performed to make the signature valid and pythonic:
-# 1) All inputs with default values will come after the inputs without default values
-# 2) The input names are converted to pythonic names (spaces and symbols replaced
-#    with underscores and letters lowercased).
-
-#output_model_uri_template = os.path.join(, kfp.dsl.EXECUTION_ID_PLACEHOLDER, 'output_model_uri', 'data')
 # Define a pipeline and create a task from a component:
-@kfp.dsl.pipeline(name='Exploring Reusability pipeline', description='Pipeline to try out reusable components')
-def my_pipeline():
+@kfp.dsl.pipeline(
+    name='Exploring Reusability pipeline', 
+    description='Pipeline to try out reusable components')
+def my_pipeline(
+    dense_nodes: int = 1024,
+    epochs: int = 100,
+    batch_size: int = 32,
+    #lr: float = 0.00003, # TODO why does this break pipeline?
+    optimizer: str = "adam",
+    transfer_learning: bool = True,
+    verbose: int = 2
+):
     get_data_op = load_component("wget_url")
     get_data_task = get_data_op(
-        # Input name "Input 1" is converted to pythonic parameter name "input_1"
         data_url=r"https://www.robots.ox.ac.uk/~vgg/data/pets/data/images.tar.gz",
         #output=Path("data"),
     )
 
     extract_data_op = load_component("untar_data")
     extract_data_task = extract_data_op(
-        tar_file=get_data_task.outputs['output_file']
+        path_to_tar_file=get_data_task.outputs['output_file'],
+        tar_file_name='images.tar.gz'
     )
 
     clean_data_op = load_component("clean_oxford_pet_data")
     clean_data_task = clean_data_op(
-        data_dir=extract_data_task.outputs['data_dir']
+        data_dir=extract_data_task.outputs['data_dir'])
+  
+    split_op = load_component("split_oxford_pet_data")
+    split_task = split_op(
+        data_dir=clean_data_task.outputs['cleaned_data_dir'],
+        split_ratio=0.1
     )
-    
+
+    # can't use files since it can't reach it later on...
+    #train_op = kfp.components.load_component_from_file("/pipeline/component.yaml")
+    #train_op = kfp.components.load_component_from_url(
+        #"https://raw.githubusercontent.com/Benjamin-Etheredge/Gloves/master/component.yaml")
+    #train_task = train_op(
+        #train_dir=split_task.outputs['train_dir'],
+        #test_dir=split_task.outputs['test_dir'],
+        #all_dir=clean_data_task.outputs['cleaned_data_dir'],
+        #metrics_file_name="metrics.yaml",
+        #model_file_name="model.h5"
+        # hypers
+        #dense_nodes=dense_nodes,
+        #epochs=epochs,
+        #batch_size=batch_size,
+        #lr=lr,
+        #optimizer=optimizer,
+        #transfer_learning=transfer_learning,
+        #verbose=verbose,
+    #)
+    #.set_gpu_limit(1)
+      
 
 # This pipeline can be compiled, uploaded and submitted for execution.
 #kfp.Client().create_run_from_pipeline_func(my_pipeline, arguments={})
 import kfp.compiler as compiler
+# TODO cleanup absolute path
 compiler.Compiler().compile(pipeline_func=my_pipeline, package_path="/pipeline/gloves_pipeline.tar.gz")
+
+# TODO auto upload pipeline
