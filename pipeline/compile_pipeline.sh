9491814002c9e4744e15da795975de01bd3c1ecf
ben <benjamin.etheredge@gmail.com>
2021-01-20 09:50:49 -0600

    fixing env file name

--- pipeline/compile_pipeline.sh
+++ pipeline/compile_pipeline.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 full_image_name=etheredgeb/gloves:pipeline
-touch ~/.env
+touch ../.devcontainer/.env  #ensure file exists
 docker run --env-file ../.devcontainer/.env -v $(pwd):/pipeline $full_image_name python /pipeline/src/pipeline.py
