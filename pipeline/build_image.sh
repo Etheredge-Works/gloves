eca5bb52144597ba38f025f0352ee4a3341140b6
ben <benjamin.etheredge@gmail.com>
2021-01-25 10:25:20 -0600

    Updating docker stuff. Can now be used with kubeflow jupyter server.

--- pipeline/build_image.sh
+++ pipeline/build_image.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
-image_name=etheredgeb/gloves
-image_tag=pipeline
+image_name=etheredgeb/gloves_kubeflow
+image_tag=0.1
 full_image_name=${image_name}:${image_tag}
 
 cd "$(dirname "$0")" 
-docker build -t "${full_image_name}" .
-docker push "$full_image_name"
+docker build -t "${full_image_name}" -t "$image_name:latest" .
+docker push "$image_name"
 
 # Output the strict image name (which contains the sha256 image digest)
 docker inspect --format="{{index .RepoDigests 0}}" "${full_image_name}"
--- pipeline/compile_pipeline.sh
+++ pipeline/compile_pipeline.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
-full_image_name=etheredgeb/gloves:pipeline
+full_image_name=etheredgeb/gloves_kubeflow:latest
 touch ../.devcontainer/.env  #ensure file exists
+# TODO correct path
 docker run --env-file ../.devcontainer/.env -v $(pwd):/pipeline $full_image_name python /pipeline/src/pipeline.py
