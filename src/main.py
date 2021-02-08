5bbe9aabe5c72b27f3d1d72be27665b7daff5e69
ben <benjamin.etheredge@gmail.com>
2021-02-08 09:10:55 -0600

    Moving to using my library

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 #! python
 from genericpath import exists
 import os
+from src.custom_model import build_custom_encoder
 os.environ['PYTHONHASHSEED']=str(4)
 #import wandb
 #wandb.init(project="gloves", config={"hyper":"parameter"})
@@ -15,6 +16,7 @@ import utils
 from settings import MIXED_PRECISION
 import yaml
 import tensorflow_addons as tfa
+from icecream import ic
 #import mlflow
 
 
@@ -34,7 +36,6 @@ if MIXED_PRECISION:
     policy = mixed_precision.Policy('mixed_float16')
     mixed_precision.set_policy(policy)
 
-import custom_model
 #print('Compute dtype: %s' % policy.compute_dtype)
 #print('Variable dtype: %s' % policy.variable_dtype)
 
@@ -44,12 +45,16 @@ import custom_model
 def log_metric(key, value, step=None):
     mlflow.log_metric(key=key, value=value, step=step)
 
+from siamese.models import Encoder, SiameseModel
+from siamese.layers import NormDistanceLayer
+from siamese.data import create_dataset, get_labels_from_filenames
 
 @click.command()
 # File stuff
 @click.option('--train-dir', type=click.Path(exists=True), help='')
+@click.option('--train-extra-dir', default=None, type=click.Path(exists=True), help='')
 @click.option('--test-dir', type=click.Path(exists=True), help='')
-@click.option('--all-dir', type=click.Path(exists=True), help='')
+@click.option('--test-extra-dir',  default=None, type=click.Path(exists=True), help='')
 @click.option('--model-dir', type=click.Path(exists=False), help='')
 @click.option('--model-filename', type=click.STRING, help='')
 #@click.option('--encoder-dir', type=click.Path(exists=False), help='')
@@ -73,11 +78,11 @@ def log_metric(key, value, step=None):
 ###############################################################################
 # Training Stuff
 @click.option('--loss_name', default='contrastive_loss', type=click.STRING, help='')
-@click.option('--lr', type=click.FLOAT, help='')
+@click.option('--lr', default=0.00003, type=click.FLOAT, help='')
 @click.option('--optimizer', default='adam', type=click.STRING, help='')
-@click.option('--epochs', type=click.INT, help='')
-@click.option('--batch-size', type=click.INT, help='')
-@click.option('--verbose', type=click.INT, help='')
+@click.option('--epochs', default=1000, type=click.INT, help='')
+@click.option('--batch-size', default=32, type=click.INT, help='')
+@click.option('--verbose', default=1, type=click.INT, help='')
 #@click.option("--validation-ratio", default=0.3, type=float)
 @click.option("--eval-freq", default=1, type=int)
 @click.option("--reduce-lr-factor", default=0.8, type=float)
@@ -89,8 +94,9 @@ def log_metric(key, value, step=None):
 @click.option("--nways", default=16, type=int)
 def main(
         train_dir: str, 
+        train_extra_dir: str, 
         test_dir: str, 
-        all_dir: str, 
+        test_extra_dir: str, 
         model_dir: str,
         model_filename: str,
         encoder_model: str,
@@ -129,8 +135,9 @@ def main(
 
     params = {
         'train_dir': train_dir, 
+        'train_extra_dir': train_extra_dir, 
         'test_dir': test_dir, 
-        'all_dir': all_dir, 
+        'test_extra_dir': test_extra_dir, 
         'model_dir': model_dir,
         'dense_nodes': dense_nodes,
         'epochs': epochs, 
@@ -141,42 +148,22 @@ def main(
         'verbose': verbose, 
         'model_filename': model_filename,
     }
+    #ic(params)
     mlflow.log_params(params)
     
-    #custom_model.gridsearch()
-    '''
-    model, history = custom_model.create_model(
-        #train_dir=pathlib.Path('data/images'),
-        train_dir=train_dir,
-        test_dir=test_dir,
-        all_data_dir=all_dir,
-        dense_nodes=dense_nodes, 
-        epochs=epochs, 
-        batch_size=batch_size, 
-        lr=lr,
-        optimizer=optimizer, 
-        transfer_learning=transfer_learning,
-        verbose=verbose,
-        )
-    '''
-    #model = custom_model.glovesnet(dense_nodes, should_transfer_learn=transfer_learning)
-    #print(model.summary())
-    #model.compile(loss="binary_crossentropy",
-                  #optimizer=tf.keras.optimizers.Adam(learning_rate=lr), metrics=["accuracy"])
     if mixed_precision != 0:
       policy = tf.keras.mixed_precision.experimental.Policy('mixed_float16')
       tf.keras.mixed_precision.experimental.set_policy(policy)
 
-    trainable, encoder, _ = custom_model.build_model(
-        dense_layers=dense_layers, 
-        dense_nodes=dense_nodes, 
-        latent_nodes=latent_nodes, 
-        dropout_rate=dropout_rate, 
-        input_shape=(height, width, depth), 
-        should_transfer_learn=should_transfer_learn,
+    encoder = Encoder(
+        conv_activation=activation,
+        dense_layers_count=dense_layers,
+        dense_nodes=dense_nodes,
+        latent_nodes=latent_nodes,
         activation=activation,
         final_activation=final_activation)
-    encoder = siamese.models.``
+
+    model = SiameseModel(encoder, NormDistanceLayer())
     
     optimizer_switch = {
         'adam': tf.keras.optimizers.Adam
@@ -188,23 +175,78 @@ def main(
         'mse': 'mse'
     }
     loss = loss_switch[loss_name]
-    trainable.compile(loss=loss, optimizer=optimizer(lr=lr))
+
+    #encoder = build_custom_encoder(den)
+    model.compile(loss=tfa.losses.ContrastiveLoss(), optimizer=optimizer(lr=lr))
+    #model.compile(loss=loss, optimizer=optimizer(lr=lr))
 
     # TODO extract and pass in
-    train_ds, steps_per_epoch, val_ds, test_ds = utils.get_dataset_values(train_dir=train_dir,
-                                                                    test_dir=test_dir,
-                                                                    all_data_dir=all_dir,
-                                                                    batch_size=batch_size)
+    train_files_tf = tf.io.gfile.glob(str(Path(train_dir)/'**.jpg'))
+    train_labels = get_labels_from_filenames(train_files_tf)
+    ic(train_labels)
+    ic(len(train_labels))
+    #ic(tf.size(train_files_tf))
+    #ic(train_files_tf)
+    #ic(tf.size(train_labels))
+    #ic(train_labels)
+    assert len(train_files_tf) == len(train_labels)
+    assert tf.size(train_files_tf) > 0, "no train files found"
+    extra_train_files = tf.io.gfile.glob(str(Path(train_extra_dir)/'**.jpg')) \
+        if train_extra_dir \
+        else None
 
-    mlflow.log_param("dataset_size", len(list(train_ds)))
+
+    test_files_tf = tf.io.gfile.glob(str(Path(test_dir)/'**.jpg'))
+    ic(tf.size(test_files_tf))
+    ic(len(test_files_tf))
+    test_labels = get_labels_from_filenames(test_files_tf)
+    ic(test_labels)
+    ic(len(test_labels))
+    assert len(test_files_tf) == len(test_labels)
+    assert tf.size(test_files_tf) > 0, "no test files found"
+    #ic(test_labels)
+    extra_test_files = tf.io.gfile.glob(str(Path(test_extra_dir)/'**.jpg')) \
+        if test_extra_dir \
+        else None
+
+    #all_files_tf  = tf.concat([train_files_tf, test_files_tf])
+    ds = create_dataset(
+        anchor_items=train_files_tf,
+        anchor_labels=train_labels,
+        anchor_decode_func=utils.read_decode,
+        #other_items=extra_train_files,
+        #other_labels=get_labels_from_files_path(extra_train_files),
+    )
+    #ds = ds.map(lambda anchor, other, label: prepr)
+    val_ds = create_dataset(
+        anchor_items=test_files_tf,
+        anchor_labels=test_labels,
+        #anchor_items=train_files_tf,
+        #anchor_labels=train_labels,
+        anchor_decode_func=utils.read_decode,
+        #other_items=train_files_tf, # needed since test set won't have many items
+        #other_labels=train_labels
+    )
+
+    mlflow.log_param("dataset_size", len(list(ds)))
     mlflow.log_param("validation_dataset_size", len(list(val_ds)))
 
     #wandb.init(project="siamese")
     #model.fit([pairs_train[:,0], pairs_train[:,1]], labels_train[:], batch_size=128, epochs=20, callbacks=[WandbCallback()])
+    #ic(ds.take(2))
+    #for (i, j), k in ds.take(1):
+        #ic(i,j,k)
+    #anchor, other, label = ds.take(1)[0]
+    #ic(anchor, other, label)
+    ds = ds.batch(batch_size)
+    val_ds = val_ds.batch(batch_size)
+    val_ds = val_ds.cache() # want a consistent val_ds
+    #model.predict(ds.take(3))
 
-    train_hist = trainable.fit(
-        train_ds,
+    train_hist = model.fit(
+        ds,
         epochs=epochs,
+        batch_size=batch_size,
         validation_data=val_ds,
         validation_freq=eval_freq,
         #steps_per_epoch=steps_per_epoch,
@@ -219,6 +261,8 @@ def main(
                                                 #mode='max', save_best_only=True)
         ]
     )
+    model.summary()
+    encoder.summary()
 
     history_dict = train_hist.history
     history_dict = {key: float(value[-1]) for key, value in history_dict.items()}
@@ -229,7 +273,7 @@ def main(
 
     #model.save("model", save_format='tf')
     Path(model_dir).mkdir(parents=True)
-    trainable.save(str(Path(str(model_dir))/model_filename))
+    model.save(str(Path(str(model_dir))/model_filename))
     #trainable.save(str(Path(str(model_dir))/encoder_model))
     encoder_path = Path(str(encoder_model))
     encoder_path.parent.mkdir(parents=True, exist_ok=True)
