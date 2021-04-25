61d91dcaf515cd43bb12e5299a3efe1071f8a845
ben <benjamin.etheredge@gmail.com>
2021-04-25 15:35:46 -0500

    Upgraded classifier application.

--- gui/src/classifier_app.py
+++ gui/src/classifier_app.py
@@ -12,11 +12,16 @@ import tensorflow as tf
 import numpy as np
 import mlflow
 from pathlib import Path
+import joblib
+from icecream import ic
+import utils
+import pandas as pd
+import datetime
 
 #model = custom_model.get_model()
 
 st.title("""
-Pet Bread Classifer
+Pet Breed Classifier
 
 This is a demo site for my Siamese Network experiments in image comparisons.
 This page demonstrates the effectiveness of the current siamese network encoder applied to feature extraction for pet breed classifiction on the Oxford pet dataset. 
@@ -28,67 +33,79 @@ Process:
    b. Siamese network architecutre is used for feature extraction with same dense network architecture but is randomly initialized instead of being frozen. (measures effectivness of network pre-training)
 """)
 
-client = mlflow.tracking.MlflowClient()
-MODEL_NAME='gloves-classifer-'
-MODEL_STAGE='Production'
-#model = mlflow.keras.load_model(model_uri=f"models:/{MODEL_NAME}/{MODEL_STAGE}")
-model_version = client.get_latest_versions(name=MODEL_NAME, stages=[MODEL_STAGE])[0]
 
-model_artifact = client.download_artifacts(model_version.run_id, 'model', dst_path='/tmp/')
-model = tf.keras.models.load_model('/tmp/model')
+@st.cache(allow_output_mutation=True)
+def load_models(model_name, mlflow_model_stage='Production'):
+   client = mlflow.tracking.MlflowClient()
+   model_version = client.get_latest_versions(name=f"gloves_{model_name}", stages=[mlflow_model_stage])[0]
 
-st.cache()
-anchor_file = st.file_uploader("Input an Image", type="jpg",)
-if anchor_file is not None:
-   st.image(anchor_file, caption="Uploaded Anchor Image.", use_column_width=True)
 
-other_file_1 = st.file_uploader("Input Image 1 to compare to Anchor Image", type="jpg")
-if other_file_1 is not None:
-   st.image(other_file_1, caption="Uploaded Other Image 1.", use_column_width=True)
+   #st.write(client.get_run(model_version.run_id.mlflow.parentRunId))
+   ic('--------------')
+   rundata = client.get_run(model_version.run_id)
+   parent_id = rundata.data.tags['mlflow.parentRunId']
+   parentrundata = client.get_run(parent_id)
+   ic(parentrundata)
+   model_path = client.download_artifacts(model_version.run_id, model_name, dst_path='/tmp/')
+   model = tf.keras.models.load_model(f"{model_path}/data/model.h5")
 
-other_file_2 = st.file_uploader("Input Image 2 to compare to Anchor Image", type="jpg")
-if other_file_2 is not None:
-   st.image(other_file_2, caption="Uploaded Other Image 2.", use_column_width=True)
-#st.write(Image.frombytes('RGB', anchor_file))
-
-if anchor_file and other_file_1 and other_file_2:
-   st.write("Processing Images...")
-   cleaned_anchor = utils.simple_decode(anchor_file.read())
-   cleaned_other_1 = utils.simple_decode(other_file_1.read())
-   cleaned_other_2 = utils.simple_decode(other_file_2.read())
-
-   #cleaned_anchor = utils.decode_img(tf.io.decode_raw(anchor_file, 'uint8'))
-   #cleaned_other = utils.decode_img(tf.io.decode_raw(anchor_file, 'uint8'))
-   st.write("Done.")
-
-   #prediction_value = st.empty()
-
-   #st.write(f"Predicion: {prediction}")
-   prediction_value_1 = model.predict([np.expand_dims(cleaned_anchor, axis=0), np.expand_dims(cleaned_other_1, axis=0)])[0][0]
-   prediction_value_2 = model.predict([np.expand_dims(cleaned_anchor, axis=0), np.expand_dims(cleaned_other_2, axis=0)])[0][0]
-   col1, col2 = st.beta_columns(2)
-   if prediction_value_1 < prediction_value_2:
-      st.write("Image 1 is closer to anchor")
-      #st.image([anchor_file, other_file_1], caption=['Anchor', 'Other'])
-      #other_file_2 = Image.open(other_file_2).c
-      other_file = other_file_1
-   else:
-      st.write("Image 2 is closer to anchor")
-      #st.image([anchor_file, other_file_2], caption=['Anchor', 'Other'], use_column_width=True)
-      other_file = other_file_2
-
-   col1.image(anchor_file, caption='Anchor')
-   col2.image(other_file_1, caption='Match')
-   #col3.image(other_file_2, caption='Other 2')
-   #if prediction_value < 0.5:
-      #st.write("These are NOT the same animal.")
-   #else:
-      #st.write("These are the same animal.")
-   st.write(f"Prediction value_1: {prediction_value_2}")
-   st.write(f"Prediction value_2: {prediction_value_2}")
+   label_encoder_path = client.download_artifacts(model_version.run_id, 'label_encoder', dst_path='/tmp/')
+   label_encoder = joblib.load(label_encoder_path)
    
-   #prediction_value.text(model.predict([np.expand_dims(cleaned_anchor, axis=0), np.expand_dims(cleaned_other, axis=0)]))
-   #st.write(model.predict([np.expand_dims(cleaned_anchor, axis=0), np.expand_dims(cleaned_other, axis=0)]))
+   return model, label_encoder, model_version
+
+
+#def predict(model, anchor, other):
+   #return model.predict([np.expand_dims(anchor, axis=0), np.expand_dims(other, axis=0)])[0][0]
+model_names = [
+   "encoder_frozen",
+   "encoder_unfrozen",
+   "imagenet_frozen",
+   "imagenet_unfrozen",
+]
+models = [(model_name, load_models(model_name)) for model_name in model_names]
+
+
+#model, le = load_models()
+image = st.file_uploader("Input Image to classify", type="jpg")
+if image is not None:
+   st.image(image, caption="Uploaded Image.", use_column_width=True)
+n = st.number_input("Top Labels to get", value=5, max_value=32, step=1)
+
+cols = st.beta_columns(2)
+
+st.write("# Apendix")
+if image is not None:
+   data = utils.simple_decode(image.read())
+   for idx, (name, (model, le, model_version)) in enumerate(models):
+      y_hat = model.predict([np.expand_dims(data, axis=0)])[0]
+      sorted_predictions = np.argsort(y_hat)[::-1]
+      predictions = le.inverse_transform(sorted_predictions)
+      #cols[idx].write(f"{name}\n\n{predictions[:n]}")
+      df = pd.DataFrame({
+         "Breed": predictions[:n],
+         "Confidence": y_hat[sorted_predictions[n]]})
+      
+      #df.set_index('Breed', inplace=True)
+
+      cols[idx%2].write(name)
+      cols[idx%2].dataframe(df)
+      with st.beta_expander(f"{name} model info"):
+         st.write(f"""
+            | Model Creation Date |  Model Version |
+            | :-------: | :---: |
+            | {datetime.datetime.fromtimestamp(float(model_version.creation_timestamp/1000)).strftime('%Y-%m-%d %H:%M:%S.%f')} | {model_version.version} |
+            ## Summary
+         """)
+         model.summary(print_fn=st.text)
+
+st.write("""
+# General Notes/TODOs
+- Validation accuracy isn't perfect as there's a chance validation samples were leaked during training of siamese network
+- K fold (or another variant) of cross-validation is needed. Currently that's non-trivial to implement using tf.data.Dataset.
+   - maybe don't use tf.data.Datase.
+
+""")
 
 #st.text(prediction_value)
 #st.help(prediction_value)
